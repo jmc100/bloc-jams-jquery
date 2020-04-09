c6fb6c417e3ccb187fcc0b32ae4058db6a5d0f75
jmc100 <40350472+jmc100@users.noreply.github.com>
2020-04-09 10:54:30 +0800

    Update player-bar.js

---
+++ scripts/player-bar.js
@@ -0,0 +1,31 @@
+{
+  $('button#play-pause').on('click', function() {
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
+
+  // next button
+  $('button#next').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    const nextSong = album.songs[nextSongIndex];
+    if (nextSongIndex >= album.songs.length) { return; }
+
+    player.playPause(nextSong);
+  });
+
+  // previous button
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    const prevSong = album.songs[prevSongIndex];
+    if (prevSongIndex < 0) { return; }
+
+    player.playPause(prevSong);
+  });
+
+}
