4f670d8ed6ea2a66e7e83d9660b785dfe689365f
jmc100 <40350472+jmc100@users.noreply.github.com>
2020-04-10 01:48:46 +0800

    Update helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,9 +1,9 @@
 class Helper {
   playPauseAndUpdate(song) {
-    player.playPause(song);
+    helper.playPauseAndUpdate(song);
     $(this.'#time-control .total-time') = player.getDuration();
     $('#time-control .total-time').text(totalTime);
-    $(player.playPause).replaceWith(helper.playPauseAndUpdate);
+
   };
 }
 
